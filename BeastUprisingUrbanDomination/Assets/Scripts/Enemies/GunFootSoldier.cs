e98749f42fea23934359f04beaff77ade30265fc
Ankit-Saha-2003 <92501201+Ankit-Saha-2003@users.noreply.github.com>
2024-09-28 02:03:07 +0530

    Revert "Add BGM and gunshot sounds"
    This reverts commit c25364bfb2b2b32904b02294575c73a0545318d0.

--- BeastUprisingUrbanDomination/Assets/Scripts/Enemies/GunFootSoldier.cs
+++ BeastUprisingUrbanDomination/Assets/Scripts/Enemies/GunFootSoldier.cs
@@ -7,8 +7,6 @@ public class GunFootSoldier : Enemy
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] GameObject bulletSpawnPoint;
 
-    public AudioSource[] gunshots;
-
     new void Start()
     {
         base.Start();
@@ -23,9 +21,5 @@ public class GunFootSoldier : Enemy
     {
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, Quaternion.identity);
         bullet.GetComponent<GunBullet>().direction = new Vector2(targetObject.transform.position.x - transform.position.x, 0).normalized;
-        
-        int gunshotChoice = Random.Range(0, gunshots.Length);
-        AudioSource gunshot = gunshots[gunshotChoice];
-        gunshot.PlayOneShot(gunshot.clip);
     }
 }
